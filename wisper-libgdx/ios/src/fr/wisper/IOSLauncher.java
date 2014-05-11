a9fd0941befada8b0b54f480cb18e967e2fd5f3d
Kévin Renella <kevin.renella@gmail.com>
2014-05-11 19:10:49 +0200

    libgdx base

---
+++ wisper-libgdx/ios/src/fr/wisper/IOSLauncher.java
@@ -0,0 +1,22 @@
+package fr.wisper;
+
+import org.robovm.apple.foundation.NSAutoreleasePool;
+import org.robovm.apple.uikit.UIApplication;
+
+import com.badlogic.gdx.backends.iosrobovm.IOSApplication;
+import com.badlogic.gdx.backends.iosrobovm.IOSApplicationConfiguration;
+import fr.wisper.WisperGame;
+
+public class IOSLauncher extends IOSApplication.Delegate {
+    @Override
+    protected IOSApplication createApplication() {
+        IOSApplicationConfiguration config = new IOSApplicationConfiguration();
+        return new IOSApplication(new WisperGame(), config);
+    }
+
+    public static void main(String[] argv) {
+        NSAutoreleasePool pool = new NSAutoreleasePool();
+        UIApplication.main(argv, null, IOSLauncher.class);
+        pool.close();
+    }
+}

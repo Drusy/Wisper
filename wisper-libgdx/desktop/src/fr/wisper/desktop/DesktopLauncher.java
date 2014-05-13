fb341a5db0f6dcac90ad50ce94c93aa17a985301
Kévin Renella <kevin.renella@gmail.com>
2014-05-13 23:01:57 +0200

    Debug log + sprite animation test

--- wisper-libgdx/desktop/src/fr/wisper/desktop/DesktopLauncher.java
+++ wisper-libgdx/desktop/src/fr/wisper/desktop/DesktopLauncher.java
@@ -1,12 +1,20 @@
 package fr.wisper.desktop;
 
+import com.badlogic.gdx.Files;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import fr.wisper.WisperGame;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+
+        config.title = "Wisper";
+
+        config.addIcon("icons/wisper_16_windows.png", Files.FileType.Local);
+        config.addIcon("icons/wisper_32_pc.png", Files.FileType.Local);
+        config.addIcon("icons/wisper_128_mac.png", Files.FileType.Local);
+
 		new LwjglApplication(new WisperGame(), config);
 	}
 }
